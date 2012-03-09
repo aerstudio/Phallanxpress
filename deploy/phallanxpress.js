c437263ca27dbb1e2de36ae72c04d114e4c22f74
hector spc <hector@aerstudio.com>
2012-03-09 13:29:47 +0100

    Built with last changes

--- deploy/phallanxpress.js
+++ deploy/phallanxpress.js
@@ -31,32 +31,31 @@
     root.Phallanxpress = previousPhallanxpress;
     return this;
   };
-  Phallanxpress.Storage = (function() {
-    Storage.prototype.expireTime = 24;
-    function Storage(name, session) {
+  Phallanxpress.Cache = (function() {
+    Cache.prototype.expireTime = 24;
+    function Cache(name) {
       this.name = name;
-      if (session == null) {
-        session = false;
-      }
-      this.enable(session)();
+      _.extend(this, Backbone.Events);
+      this.enable();
+      this.objects = (JSON.parse(this.storage.getItem(this.name))) || [];
     }
-    Storage.prototype.enable = function(session) {
-      if (session == null) {
-        session = false;
+    Cache.prototype.enable = function(options) {
+      if (options == null) {
+        options = {};
       }
       if (!(typeof window !== "undefined" && window !== null) && !window.localStorage && !window.sessionStorage) {
         return;
       }
-      if (session) {
+      if (options.sessionStorage) {
         return this.storage = window.sessionStorage;
       } else {
         return this.storage = window.localStorage;
       }
     };
-    Storage.prototype.disable = function() {
+    Cache.prototype.disable = function() {
       return this.storage = null;
     };
-    Storage.prototype.saveCollection = function(collection) {
+    Cache.prototype.saveCollection = function(collection) {
       var col;
       if (collection.length === 0 || !(this.storage != null)) {
         return;
@@ -67,19 +66,21 @@
       };
       try {
         this.storage.setItem(collection.url, JSON.stringify(col));
+        collection.each(__bind(function(model) {
+          return this.saveModel(model);
+        }, this));
+        return this.saveId(collection.url);
       } catch (error) {
         if (error === QUOTA_EXCEEDED_ERR) {
-          return;
+          this.cleanStorage();
+          this.trigger('quota exceeded');
         } else {
           throw error;
         }
       }
-      return collection.each(__bind(function(model) {
-        return this.saveModel(model);
-      }, this));
     };
-    Storage.prototype.saveModel = function(model) {
-      var now;
+    Cache.prototype.saveModel = function(model) {
+      var id, now;
       if (this.storage == null) {
         return;
       }
@@ -89,14 +90,19 @@
         silent: true
       });
       try {
-        return this.storage.setItem(this.name + model.constructor.name.toLowerCase() + '/' + model.id, JSON.stringify(model));
+        id = this.name + model.constructor.name.toLowerCase() + '/' + model.id;
+        this.storage.setItem(id, JSON.stringify(model));
+        return this.saveId(id);
       } catch (error) {
-        if (error === QUOTA_EXCEEDED_ERR) {} else {
+        if (error === QUOTA_EXCEEDED_ERR) {
+          this.cleanStorage();
+          this.trigger('quota exceeded');
+        } else {
           throw error;
         }
       }
     };
-    Storage.prototype.getCollection = function(collection) {
+    Cache.prototype.getCollection = function(collection) {
       var col, elapsedTime, modelName, models;
       if (this.storage == null) {
         return false;
@@ -121,14 +127,13 @@
           }
           return true;
         } else {
-          this.destroyCollection(collection);
           return false;
         }
       } else {
         return false;
       }
     };
-    Storage.prototype.getModel = function(model) {
+    Cache.prototype.getModel = function(model) {
       var attr, elapsedTime, timestamp;
       attr = this.getModelAttributes(model.id, model.constructor.name);
       if (!((attr != null) || (this.storage != null))) {
@@ -150,16 +155,44 @@
         return false;
       }
     };
-    Storage.prototype.getModelAttributes = function(id, modelName) {
+    Cache.prototype.getModelAttributes = function(id, modelName) {
+      if (this.storage == null) {
+        return null;
+      }
       return JSON.parse(this.storage.getItem(this.name + modelName.toLowerCase() + '/' + id));
     };
-    Storage.prototype.destroyCollection = function(collection) {
+    Cache.prototype.saveId = function(id) {
       if (this.storage == null) {
         return;
       }
-      return this.storage.removeItem(collection.url);
+      if (!_.include(this.objects, id)) {
+        this.objects.push(id);
+        return this.storage.setItem(this.name, JSON.stringify(this.objects));
+      }
     };
-    return Storage;
+    Cache.prototype.cleanStorage = function(forceCleanAll) {
+      var removedIds;
+      if (forceCleanAll == null) {
+        forceCleanAll = false;
+      }
+      removedIds = [];
+      _.each(this.objects, __bind(function(id) {
+        var data, elapsedTime;
+        data = JSON.parse(this.storage.getItem(id));
+        elapsedTime = new Date().getTime() - (data.timestamp || data.phallanxTimestamp);
+        if (elapsedTime > this.expireTime * 3600000 || forceCleanAll) {
+          this.storage.removeItem(id);
+          return removedIds.push(id);
+        }
+      }, this));
+      this.objects = _.difference(this.objects, removedIds);
+      if (this.objects.length > 0) {
+        return this.storage.setItem(this.name, JSON.stringify(this.objects));
+      } else {
+        return this.storage.removeItem(this.name);
+      }
+    };
+    return Cache;
   })();
   Phallanxpress.Api = (function() {
     function Api(url) {
@@ -180,7 +213,7 @@
           $('head').append(dnsprefetch);
         }
       }
-      this.cache = new Phallanxpress.Storage(this.url);
+      this.cache = new Phallanxpress.Cache(this.url);
     }
     Api.prototype.recentPosts = function(options) {
       var posts, view;
